c84df0f492c1e295f47d0edfcb38c4f12c7fed3f
Jimmy Tran <jimmytran1620@gmail.com>
2021-06-27 00:18:01 -0400

    added rePositionServoMotor function, need to figure out turnOn and turnOff positions

--- src/light-switch-sensor.ino
+++ src/light-switch-sensor.ino
@@ -1,76 +1,74 @@
 #include <Servo.h>
 
-// status of light
-bool isLightOn = false;
-bool isLightOff = true;
-
 // constants
-const int lightPin = A0;   // Analog pin for CdS Photoresistor (light sensor)
-const int motionPin = 7;  // Pin for input from PIR Sensor (motion sensor)
-const int nightLight = 13;    // Pin for output to light bulb
+const int lightPin = A0; // Analog pin for CdS Photoresistor (light sensor)
+const int motionPin = 7; // Pin for input from PIR Sensor (motion sensor)
+const int nightLight = 13; // Pin for output to light bulb
 const int servoPin = 8; // Pin for the servo device
 
 const int initialPosition = 90; // initial position for the servo
 const int turnOnPosition = 85; // position of the servo to turn on the light
 const int turnOffPosition = 95; // position of the servo to turn off the light
 
+// status of light
+bool isLightOn = false;
+bool isLightOff = true;
+
 // Servo instance
 Servo servoMotorInstance;
 
 void setup() {
   Serial.begin(9600);
-  
   pinMode(nightLight, OUTPUT);
   pinMode(lightPin, INPUT);
   pinMode(motionPin, INPUT);
   
   //make sure the light is off
-  digitalWrite(nightLight, LOW); 
-	
+  digitalWrite(nightLight, LOW);
   // configure the Servo motor
   servoMotorInstance.attach(servoPin);
   // set the servo to be at 180 degrees
   servoMotorInstance.write(initialPosition);
 }
-   
+
 void loop() {
-  	// get the reading of the motion detector
-  	int motionReading = digitalRead(motionPin); 
-  	
-	// condition to check if any motion was detected
-  	if (motionReading) {
-      if (lightPin < 200) { 
-        digitalWrite(nightLight,HIGH);
-        turnOnLight();
-      }
-    }else {
-    	digitalWrite(nightLight,LOW);
-      	turnOffLight();
+  // get the reading of the motion detector
+  int motionReading = digitalRead(motionPin);
+
+  // condition to check if any motion was detected
+  if (motionReading) {
+    if (lightPin < 200) {
+      digitalWrite(nightLight, HIGH);
+      turnOnLight();
     }
-  	// delay for 200 miliseconds
-	delay(200);
- }
+  } else {
+    digitalWrite(nightLight, LOW);
+    turnOffLight();
+  }
+  // delay for 200 miliseconds
+  delay(200);
+}
 
 // functions to turn on and off the lights
-void turnOnLight(){
+void turnOnLight() {
   if (!isLightOn) {
-    isLightOn = true;
-    isLightOff = false;
-    
-    servoMotorInstance.write(turnOnPosition);
-    delay(500);
-    servoMotorInstance.write(initialPosition);
-	Serial.println("LIGHT TURNED ON!");
+    rePositionServoMotor(turnOnPosition, true, false);
   }
 }
-void turnOffLight(){
-  if (!isLightOff){
-    isLightOff = true;
-    isLightOn = false;
-    
-    servoMotorInstance.write(turnOffPosition);
-    delay(500);
-    servoMotorInstance.write(initialPosition);
-  	Serial.println("LIGHT TURNED OFF!");
+void turnOffLight() {
+  if (!isLightOff) {
+    rePositionServoMotor(turnOffPosition, false, true);
   }
+}
+
+// function to reposition the servo motor
+void rePositionServoMotor(int position, bool lightOn, bool lightOff) {
+  servoMotorInstance.write(position);
+  isLightOn = lightOn;
+  isLightOff = lightOff;
+  delay(500);
+  servoMotorInstance.write(initialPosition);
+
+  String message = (lightOn) ? "LIGHT TURNED ON!" : "LIGHT TURNED OFF!";
+  Serial.println(message);
 }
