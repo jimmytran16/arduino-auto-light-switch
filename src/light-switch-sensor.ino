cdf00d77ee5ef5fad8a022c6fd25e7e1300ebb71
Jimmy Tran <jimmytran1620@gmail.com>
2021-07-10 00:09:27 -0400

    added function to reset motor to default position wafter it turns light on or off

--- src/light-switch-sensor.ino
+++ src/light-switch-sensor.ino
@@ -3,6 +3,7 @@
 // constants
 //const int lightPin = A0; // Analog pin for CdS Photoresistor (light sensor)
 const int motionPin = 7; // Pin for input from PIR Sensor (motion sensor)
+const int nightLight = 13; // Pin for output to light bulb
 const int servoPin = 8; // Pin for the servo device
 
 const int initialPosition = 90; // initial position for the servo
@@ -18,18 +19,24 @@ Servo servoMotorInstance;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(nightLight, OUTPUT);
+//  pinMode(lightPin, INPUT);
   pinMode(motionPin, INPUT);
+  
+  //make sure the light is off
+  digitalWrite(nightLight, LOW);
   // configure the Servo motor
   servoMotorInstance.attach(servoPin);
   // set the servo to be at 180 degrees
   servoMotorInstance.write(initialPosition);
-  Serial.println("STARTED....");
+  Serial.println("STARTED");
 }
 
 void loop() {
+//  exit(0);
   // get the reading of the motion detector
   int motionReading = digitalRead(motionPin);
+  Serial.println(motionReading);
 
   // condition to check if any motion was detected
   if (motionReading == 1) {
@@ -47,11 +54,15 @@ void loop() {
 void turnOnLight() {
   if (!isLightOn) {
     rePositionServoMotor(turnOnPosition, true, false);
+    delay(600);
+    resetMotorToDefaultPosition();
   }
 }
 void turnOffLight() {
   if (!isLightOff) {
     rePositionServoMotor(turnOffPosition, false, true);
+    delay(600);
+    resetMotorToDefaultPosition();
   }
 }
 
@@ -63,4 +74,9 @@ void rePositionServoMotor(int position, bool lightOn, bool lightOff) {
 
   String message = (lightOn) ? "LIGHT TURNED ON!" : "LIGHT TURNED OFF!";
   Serial.println(message);
+}
+
+// function to reset the position to default
+void resetMotorToDefaultPosition(){
+  servoMotorInstance.write(initialPosition);
 }
